cb84345be7a7fb9af9dff02d59e00191babab1b9
Mark Hendrickson <markmhendrickson@gmail.com>
2018-03-28 11:52:35 +0200

    Add gift and merchant analysis

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ var budgets = {
     food: process.env.FORECEIPT_ANALYZER_FOOD_BUDGET
   },
   monthlyTransactions = {},
-  recurringMerchants = [];
+  recurringMerchants = [],
+  merchants = [];
 
 var getRecurringMerchants = (done) => {
   var lineReader = readline.createInterface({
@@ -56,22 +57,37 @@ var analyzeMonthlyTransactions = (done) => {
 
     let transactions = monthlyTransactions[month],
       discretionaryAmount = 0,
-      foodAmount = 0;
+      foodAmount = 0,
+      giftAmount = 0;
 
     transactions.forEach((transaction) => {
       if (transaction.category) {
         if (transaction.category.includes('Food & Dining')) {
           foodAmount += new Number(transaction.amount);
-        } else if (!transaction.category.includes('Travel->Air Travel') && !transaction.category.includes('Travel->Hotel')) {
-          discretionaryAmount += new Number(transaction.amount);
+          merchants.push(transaction.merchant);
+        } else {
+          if (!transaction.category.includes('Travel->Air Travel') && !transaction.category.includes('Travel->Hotel')) {
+            discretionaryAmount += new Number(transaction.amount);
+            merchants.push(transaction.merchant);
+          }
+
+          if (transaction.category.includes('Gift')) {
+             giftAmount += new Number(transaction.amount);
+             merchants.push(transaction.merchant);
+           }
         }
       }
     });
 
     console.log('Total transactions: ' + transactions.length);
+    console.log('Total spent: €' + Math.round(discretionaryAmount + foodAmount));
     console.log(`Total discretionary amount: €${Math.round(discretionaryAmount)} (${Math.round(discretionaryAmount / budgets['discretionary'] * 100)}%)`);
+    console.log(`  Non-gifts: €${Math.round(discretionaryAmount - giftAmount)}`);
+    console.log(`  Gifts:     €${Math.round(giftAmount)}`);
     console.log(`Total food amount: €${Math.round(foodAmount)} (${Math.round(foodAmount / budgets['food'] * 100)}%)`, '\n');
   });
+
+  //console.log('Merchants', _.uniq(merchants).join('\n'));
 
   done();
 };
